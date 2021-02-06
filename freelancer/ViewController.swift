f53bdaccb1dfbfed66442617485e9716a9021ecd
Ashlee <ashleetheprogrammer@gmail.com>
2021-02-06 10:25:56 +0000

    first

---
+++ freelancer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  freelancer
+//
+//  Created by Ashlee Muscroft on 05/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
